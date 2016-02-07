3bf55017b13bbc31975442a6cb3c0d714901caf7
RyanG <decatf@gmail.com>
2016-02-07 14:47:44 -0500

    Don't copy cbd for twrp

--- p4tmo.mk
+++ p4tmo.mk
@@ -12,8 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/ril/cbd:root/sbin/cbd
+#PRODUCT_COPY_FILES += \
+#    $(LOCAL_PATH)/ril/cbd:root/sbin/cbd
 
 # Override
 PRODUCT_COPY_FILES += \
