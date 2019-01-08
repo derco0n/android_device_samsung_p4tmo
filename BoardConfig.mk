2a92dce72251b22b80e80e9196889e5cb77d590f
Dennis Marx <dennis_marx@web.de>
2019-01-08 21:08:52 +0100

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,10 +27,10 @@ BOARD_USERDATAIMAGE_PARTITION_SIZE := 14200340480
 
 # Try to build the kernel
 TARGET_KERNEL_SOURCE := kernel/samsung/p4
-# TARGET_KERNEL_CONFIG := cyanogenmod_samsung_p4_usa_tmo-jb_defconfig
+TARGET_KERNEL_CONFIG := cyanogenmod_samsung_p4_usa_tmo-jb_defconfig
 # Keep this as a fallback
 # TARGET_PREBUILT_KERNEL := device/samsung/p4tmo/kernel
-# TARGET_PREBUILT_KERNEL := device/samsung/p4tmo/kernel_twrp
+TARGET_PREBUILT_KERNEL := device/samsung/p4tmo/kernel_twrp
 
 # RIL
 BOARD_RIL_CLASS := ../../../device/samsung/p4/ril/
